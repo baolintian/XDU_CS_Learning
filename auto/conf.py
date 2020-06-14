1c728deccc2385d72715b761391a1266df04b726
baolintian <tianbaolin1@gmail.com>
2020-06-15 01:10:32 +0800

    fix failing

--- auto/conf.py
+++ auto/conf.py
@@ -53,8 +53,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-	'recommonmark', 
-	'sphinx_markdown_tables'
+	'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
