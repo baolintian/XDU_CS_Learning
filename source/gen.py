404fef20f0f08315af997aa735bf19d0825b7208
baolintian <tianbaolin1@gmail.com>
2020-06-19 07:13:12 +0800

    add contest part

--- source/gen.py
+++ source/gen.py
@@ -61,10 +61,9 @@ def get_semester(cur):
     files, dirs = get_all(cur)
     if cur == "./CS" or cur == "./SE":
         files = ['README.md', '大一上.md', '大一下.md', '大二上.md', '大二下.md', '大三上.md', '大三下.md', '大四.md']
-    
-    for i in range(len(files)):
-        files[i] = cur+"/"+files[i]
-        print("233", files[i], cur)
+        for i in range(len(files)):
+            files[i] = cur+"/"+files[i]
+
     for i in files:
         if i.endswith(EXT):
             md += f'{open(i).read()}\n\n'
